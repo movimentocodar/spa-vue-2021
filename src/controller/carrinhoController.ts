a59f736663c585a38c7466c9611942beb021dc85
Hugo Ferreira <hugoogle@terra.com.br>
2022-01-24 14:43:38 -0300

    adicionado o controler carrinho

---
+++ src/controller/carrinhoController.ts
@@ -0,0 +1,51 @@
+import ProdutoModel from "../model/Produto";
+import ProdutosDTO from "../model/Produto";
+import { Movimento } from "../model/enum";
+
+export abstract class Carrinho {
+  private static qtdTotal = 0;
+  private static vlrTotal = 0;
+
+  private static produtos: ProdutosDTO[] = [];
+
+  public static adiciona(produto: ProdutosDTO):void {
+    this.produtos.push(produto);
+  }
+
+  public static lista(): readonly ProdutosDTO[] {
+    return this.produtos;
+  }
+
+  public static quantidadeTotal(produto: ProdutoModel, movimento: Movimento): number {
+    if (movimento == Movimento.CREDITO) {
+      this.qtdTotal += produto.quantidade;
+    } else {
+      this.qtdTotal -= produto.quantidade;
+    }
+    return this.qtdTotal;
+  }
+
+  public static valorTotal(produto: ProdutoModel, movimento: Movimento): number {
+    if (movimento == Movimento.CREDITO) {
+      this.vlrTotal += parseFloat(produto.preco);
+    } else {
+      this.vlrTotal -= parseFloat(produto.preco);
+    }
+
+    return parseFloat(this.vlrTotal.toFixed(2));
+  }
+
+  public static remover(produto: ProdutosDTO):void {
+    const index = this.produtos.indexOf(produto);
+    if (index > -1) {
+      this.produtos.splice(index, 1);
+    }
+  }
+
+  public static finalizar():void{
+      this.produtos.splice(0, this.produtos.length)
+      this.qtdTotal = 0
+      this.vlrTotal = 0
+  }
+
+}
