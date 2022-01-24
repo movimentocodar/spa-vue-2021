89834105c533f9f1c2db70722541dca8837f6580
Hugo Ferreira <hugoogle@terra.com.br>
2022-01-24 14:45:26 -0300

    modificado a estrutura para ts

---
+++ src/main.ts
@@ -0,0 +1,28 @@
+import '@babel/polyfill'
+import 'mutationobserver-shim'
+import Vue from "vue";
+import './plugins/bootstrap-vue'
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+import './assets/css/reset.css';
+import axios from 'axios'
+import VueAxios from 'vue-axios'
+import VueModal from '@kouts/vue-modal'
+
+import '@kouts/vue-modal/dist/vue-modal.css'
+
+
+Vue.config.productionTip = false;
+Vue.use(VueAxios, axios, VueModal)
+
+Vue.component('Modal', VueModal)
+
+import Vuex from 'vuex'
+Vue.use(Vuex)
+
+new Vue({
+  router,
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
