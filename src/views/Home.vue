2378fd6e97e975b831b5d25a3f2bc4cf21c28b26
Hugo Ferreira <hugoogle@terra.com.br>
2022-01-24 14:28:32 -0300

    lista de navegadores e dependencia de regra de validacao de sintaxe

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,99 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+    <section class="produtos">
+      <div class="navegacao">
+        <p>Onde você está > Departamento:</p>
+        <p data-navegacao>{{ valorDepartamento }}</p>
+      </div>
+
+      <h2>Destaque</h2>
+
+      <div class="listaProdutos" data-lista-produtos>
+        <Cards></Cards>
+      </div>
+    </section>
+
+    <section class="carrinho" data-secao-carrinho>
+      <Carrinho></Carrinho>
+    </section>
+
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" /> -->
   </div>
 </template>
 
-<script>
-// @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Cards from "../components/Cards.vue";
+import Carrinho from "../components/Carrinho.vue";
+import { EventBus } from "../eventBus";
 
-export default {
-  name: 'Home',
+@Component({
   components: {
-    HelloWorld
+    Cards,
+    Carrinho,
+  },
+})
+export default class Home extends Vue {
+  valorDepartamento = "Todos";
+
+  methods(): void {
+    this.botaoDepartamento;
+  }
+
+  created(): void {
+    const _this = this;
+
+    EventBus.$on("botaoDepartamento", function(valor: string) {
+      _this.valorDepartamento = valor;
+    });
+  }
+
+  botaoDepartamento(valor: string): void {
+    this.valorDepartamento = valor;
   }
 }
 </script>
+
+<style scoped>
+.container {
+  display: flex;
+  margin-top: 1%;
+  justify-content: space-around;
+}
+
+.produtos {
+  width: 85%;
+  padding: 5px;
+}
+
+.navegacao p:last-child {
+  text-transform: capitalize;
+}
+
+h2 {
+  font-size: 25px;
+  padding: 20px;
+}
+.navegacao p {
+  padding-top: 10px;
+}
+
+.navegacao p:last-child {
+  font-weight: bold;
+}
+.navegacao {
+  display: flex;
+}
+
+.listaProdutos {
+  display: flex;
+  flex-wrap: wrap;
+  margin: 12px;
+}
+
+.carrinho {
+  width: 25%;
+  padding: 0px 0px 10px 0px;
+}
+</style>
