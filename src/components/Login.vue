73fe602fd97fe92c3a353472958ae323181b3888
Hugo Ferreira <hugoogle@terra.com.br>
2022-02-02 17:56:31 -0300

    refatorado area login para utilizar vuex

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,55 +1,107 @@
 <template>
-  <div>
-    <b-form @submit.prevent="logar($event)">
-      <p class="erro">{{ mensagem }}</p>
+  <div class="login">
+    <svg v-if="usuario.avatar_url == ''" alt="user"></svg>
+    <img else :src="usuario.avatar_url" />
 
-      <b-form-group label="Username:">
-        <b-form-input
-          v-model="login"
-          type="text"
-          required
-          placeholder="Usuario do GitHub"
-        ></b-form-input>
-      </b-form-group>
-      <br />
-      <b-button type="submit" class="submit" variant="primary">Entrar</b-button>
-      <br />
-      <br />
-      <b-alert show>Informe o seu login do GitHub</b-alert>
-    </b-form>
+    <a href="" @click.prevent="modalOpen(true)">
+      {{ usuario.name == "" ? "Login" : usuario.name }}</a
+    >
+
+    <Modal
+      :based-on="modalisShown"
+      title="Hugoogle Supermercado"
+      @close="modalisShown = modalOpen(false)"
+    >
+      <b-form @submit.prevent="logar">
+        <p class="erro">{{ mensagem }}</p>
+
+        <b-form-group label="Username:">
+          <b-form-input
+            v-model="user"
+            type="text"
+            required
+            placeholder="Usuario do GitHub"
+          ></b-form-input>
+        </b-form-group>
+        <br />
+        <b-button type="submit" class="submit" variant="primary"
+          >Entrar</b-button
+        >
+        <br />
+        <br />
+        <b-alert show>Informe o seu login do GitHub</b-alert>
+      </b-form>
+    </Modal>
   </div>
 </template>
 <script lang="ts">
+import Usuario from "@/model/Usuario";
 import { Component, Vue } from "vue-property-decorator";
-import Usuario from "../model/Usuario";
-import userServices from "../services/userServices";
+import StoreLogin from "../store/StoreLogin";
 
 @Component
 export default class Login extends Vue {
-  private login = "";
-  private usuario: Usuario;
-  private hideModal: false;
-  private mensagem = "";
+  private user = "";
+  private showModal = false;
 
-  async logar(): Promise<void> {
-    return await userServices
-      .getUser(this.login)
-      .then((response) => {
-        this.usuario = response.data;
-        if (this.usuario.name == "" || this.usuario.name == null) {
-          this.mensagem = "User name invalido!";
-        } else {
-          this.$emit("usuario-git", this.usuario, this.hideModal);
-        }
-      })
-      .catch((e: Error) => {
-        this.mensagem = `${e} - Tente novamente!`;
-      });
+  get mensagem(): string {
+    return StoreLogin.getMensagem;
+  }
+
+  get usuario(): Usuario {
+    return StoreLogin.getUsuario;
+  }
+
+  get modalisShown(): boolean {
+    return StoreLogin.getModalIsShown;
+  }
+
+  set modalisShown(estado: boolean) {
+    StoreLogin.setModalIsShown = estado;
+  }
+
+  modalOpen(estado: boolean): void {
+    this.user = "";
+    StoreLogin.ModalisShowHide(estado);
+  }
+
+  logar(): void {
+    StoreLogin.logar(this.user);
   }
 }
 </script>
 <style scoped>
+.login {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
 .erro {
   color: brown;
+}
+
+a {
+  color: #156f96;
+  text-decoration: underline;
+  cursor: pointer;
+}
+
+h1 img {
+  width: 200px;
+}
+
+nav img {
+  width: 18%;
+  border-radius: 50%;
+}
+nav svg {
+  width: 48px;
+  height: 33px;
+  background: url(../assets/imagem/svg/user.svg) 0 0 no-repeat;
+}
+
+nav a {
+  padding: 0px 10px;
 }
 </style>
