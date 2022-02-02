a9a0259e431d0dbba42769f630b0652845698c00
Hugo Ferreira <hugoogle@terra.com.br>
2022-02-02 18:02:39 -0300

    modificado pois foi criado a proprio store

--- src/main.ts
+++ src/main.ts
@@ -4,15 +4,11 @@ import Vue from "vue";
 import './plugins/bootstrap-vue'
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 import './assets/css/reset.css';
 import axios from 'axios'
 import VueAxios from 'vue-axios'
 import VueModal from '@kouts/vue-modal'
-
 import '@kouts/vue-modal/dist/vue-modal.css'
-
-
 Vue.config.productionTip = false;
 Vue.config.devtools = true
 Vue.use(VueAxios, axios, VueModal)
@@ -24,6 +20,5 @@ Vue.use(Vuex)
 
 new Vue({
   router,
-  store,
   render: (h) => h(App),
 }).$mount("#app");
--- src/store/index.ts
+++ src/store/index.ts
@@ -1,37 +1,9 @@
-
-import Vue from "vue";
+import Vue from "vue"
 import Vuex from "vuex";
 
 Vue.use(Vuex);
 
-export default new Vuex.Store({
-  
-  state: {
-    quantidade: 0
-  },
-  getters: {
-    
-    quantidade: (state) => state.quantidade
+export const store =  new Vuex.Store({
 
-  },
-
-  
-  mutations: {
-
-    atualizaQuantidadeNoCarrinho(state, delta: number){
-      state.quantidade = Number(delta)
-    }
-    
-  },
-  actions: {
-
-    atualizaQuantidadeNoCarrinho(context, delta: number){
-        context.commit('atualizaQuantidadeNoCarrinho', delta)
-      }
-
-
-
-  },
-  
-   
 });
+
