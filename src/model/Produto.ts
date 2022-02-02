85376b1d08d30ee7194740e3e143cf96d2040154
Hugo Ferreira <hugoogle@terra.com.br>
2022-02-02 18:05:21 -0300

    modelo produto

--- src/model/Produto.ts
+++ src/model/Produto.ts
@@ -1,39 +1,10 @@
-export interface IProdutoModel {
-  codigo: number;
-  imagem: string;
-  descricao: string;
-  quantidade: number;
-  preco: number;
-  departamento: string;
-}
-
-export interface IProdutoDTO {
-  codigo: number;
-  imagem: string;
-  descricao: string;
-  quantidade: number;
-  preco: number;
-  departamento: string;
-}
-
-export default class ProdutoModel implements IProdutoModel {
-  constructor(
-    public codigo: number,
-    public imagem: string,
-    public descricao: string,
-    public quantidade: number,
-    public preco: number,
-    public departamento: string
-  ) {}
-
-  static criarProduto(dto: IProdutoDTO): ProdutoModel {
-    return new ProdutoModel(
-      dto.codigo,
-      dto.imagem,
-      dto.descricao,
-      dto.quantidade,
-      dto.preco,
-      dto.departamento
-    );
+export interface Produto {
+    codigo: number;
+    imagem: string;
+    descricao: string;
+    quantidade: number;
+    preco: number;
+    precoTotal: number;
+    departamento: string;
   }
-}
+  
