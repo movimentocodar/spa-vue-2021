4f86230f1c2d03a2137df4e382c1b3ec793993b9
Hugo Ferreira <hugoogle@terra.com.br>
2022-02-02 18:00:19 -0300

    removido arquivos desnecessarios

--- src/model/httpGit.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const AxiosIntance = axios.create({
-  baseURL: "https://api.github.com/",
-});
-export default AxiosIntance;
--- src/model/httpPlaceHolder.ts
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-const AxiosIntance = axios.create({
-  baseURL: "https://my-json-server.typicode.com/hugoogle/hamburgueria",
-});
-
-export default AxiosIntance;
